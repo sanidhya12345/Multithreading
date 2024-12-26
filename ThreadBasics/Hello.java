5184a2c17dd911f3cd9d28111d718566125dd2b9
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-12-26 10:18:01 +0530

    update with some comments

--- ThreadBasics/Hello.java
+++ ThreadBasics/Hello.java
@@ -3,8 +3,8 @@ package ThreadBasics;
 public class Hello {
     public static void main(String[] args) {
 
-        World world=new World();
-        world.start();
+        World world=new World(); //NEW STATE
+        world.start(); //RUNNABLE STATE
         for(;;){
             System.out.println(Thread.currentThread().getName());
         }
